042571061533cfe6ba6f66254f44640b02e5aa41
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2024-09-27 15:56:44 +0300

    add pagination on user api and change docker compose yaml file

--- user/api/views.py
+++ user/api/views.py
@@ -1,9 +1,32 @@
-from rest_framework import viewsets
-
 from .serializers import UserSerializer
 from user.models import User
 
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class UserSearchPagination(LimitOffsetPagination):
+    default_limit = 50
+    max_limit = 250
+
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.prefetch_related("driver").all()
     serializer_class = UserSerializer
+    pagination_class = UserSearchPagination
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ["name", "personal_id"]
+
+    @extend_schema(
+        summary="List all User",
+        description="Returns a list of all Users in the database.",
+        responses={200: UserSerializer},
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)

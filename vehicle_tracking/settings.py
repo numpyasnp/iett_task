845fe9d1b82c068b720075e6b46593008289aee8
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2024-09-29 21:52:01 +0300

    add refresh token life time settings & change access token life time settings

--- vehicle_tracking/settings.py
+++ vehicle_tracking/settings.py
@@ -140,7 +140,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 SPECTACULAR_SETTINGS = {
